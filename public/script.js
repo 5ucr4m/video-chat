46ae3a88f685eec6900453b02bf0b15ab35c2abd
Marcus Vinicius <5ucr4m@gmail.com>
2023-02-24 16:50:12 -0300

    change address

--- public/script.js
+++ public/script.js
@@ -22,8 +22,9 @@ showChat.addEventListener("click", () => {
 const user = prompt("Enter your name");
 
 var peer = new Peer({
-  host: '127.0.0.1',
-  port: 3030,
+  // host: '127.0.0.1',
+  // port: 3030,
+  host: 'https://video-chat-yr3q.onrender.com',
   path: '/peerjs',
   config: {
     'iceServers': [
